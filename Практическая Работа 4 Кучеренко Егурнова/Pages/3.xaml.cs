b36d1d37fa6246beceeaf47324f15fe0ccc90664
arincha123 <a_egurnova@inbox.ru>
2026-02-24 09:43:29 +0300

    начало первой страницы

---
+++ Практическая Работа 4 Кучеренко Егурнова/Pages/3.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Практическая_Работа_4_Кучеренко_Егурнова.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для _3.xaml
+    /// </summary>
+    public partial class _3 : Page
+    {
+        public _3()
+        {
+            InitializeComponent();
+        }
+    }
+}
