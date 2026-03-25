054dca6fa03b48e3c0bd9bb306b9d8a1dd83b273
arincha123 <a_egurnova@inbox.ru>
2026-03-25 13:53:53 +0300

    Проведён рефаакторинг страницы 1 приложения

---
+++ UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("771a1f92-c5cf-4e85-a741-364fa96b920b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Практическая_Работа_4_Кучеренко_Егурнова;
+using Практическая_Работа_4_Кучеренко_Егурнова.Pages;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var _1 = new _1();
+            double x = 1;
+            double y = 2;
+            double z = 3;
+            
+
+            //double sinY = Math.Sin(y);
+            //double denominator1 = 0.5 + Math.Pow(sinY, 2);
+
+            //double zSquare = Math.Pow(z, 2);
+            ////double denominator2 = 3 - zSquare / 5;
+
+            ////double numerator = 2 * Math.Cos(x - Math.PI / 6);
+            //double firstPart = numerator / denominator1;
+
+            //double fraction = zSquare / denominator2;
+            //double secondPart = 1 + fraction;
+
+            double result = ((2 * Math.Cos(x - Math.PI / 6)) / (3 - (Math.Pow(z, 2)) / 5)) * (1 + (Math.Pow(z, 2) / (3 - (Math.Pow(z, 2)) / 5)));
+
+            
+        }
+    }
+}
