b6113e9d57988b506061ba9063262bb0552e6f75
GreenSkorp <k.e.d.2007@mail.ru>
2026-03-26 13:16:20 +0300

    Тесты готовы и проверены К каждой формуле по 3 обычной проверки на работоспособность рандомных значений (в 2 формуле 9 из-за вариаций Х) Остальные тесты являются проверками на границы и неверные значения(по типу минуса под корнем) + 3 интеграционных теста(одновременная проверка всех тестов на работоспособность и обработки ошибки в следствии неверных входных данных)

--- Практическая Работа 4 Кучеренко Егурнова/Pages/2.xaml.cs
+++ Практическая Работа 4 Кучеренко Егурнова/Pages/2.xaml.cs
@@ -49,6 +49,40 @@ namespace Практическая_Работа_4_Кучеренко_Егурн�
                 return 0;
         }
 
+
+        public double funk2(double x, double y, int functionType)
+        {
+            double fx;
+
+            if (functionType == 1)
+                fx = Math.Sinh(x);
+            else if (functionType == 2)
+                fx = x * x;
+            else if (functionType == 3)
+                fx = Math.Exp(x);
+            else
+                fx = 0;
+
+            double xy = x * y;
+            double basePart = Math.Pow(fx + y, 2);
+            double result;
+
+            if (xy > 0)
+            {
+                result = basePart - Math.Sqrt(fx * y);
+            }
+            else if (xy < 0)
+            {
+                result = basePart + Math.Sqrt(Math.Abs(fx * y));
+            }
+            else
+            {
+                result = basePart + 1;
+            }
+
+            return result;
+        }
+
         private void Count_Click(object sender, RoutedEventArgs e)
         {
             try
@@ -59,23 +93,20 @@ namespace Практическая_Работа_4_Кучеренко_Егурн�
                 if (!double.TryParse(yValue.Text, out double y))
                     throw new Exception("Введите корректное значение Y");
 
-                double fx = GetSelectedFunction(x);
-                double xy = x * y;
-                double basePart = Math.Pow(fx + y, 2);
-                double result;
-
-                if (xy > 0)
-                {
-                    result = basePart - Math.Sqrt(fx * y);
-                }
-                else if (xy < 0)
-                {
-                    result = basePart + Math.Sqrt(Math.Abs(fx * y));
-                }
+                int functionType;
+                if (rbSh.IsChecked == true)
+                    functionType = 1;
+                else if (rbX2.IsChecked == true)
+                    functionType = 2;
+                else if (rbExp.IsChecked == true)
+                    functionType = 3;
                 else
-                {
-                    result = basePart + 1;
-                }
+                    throw new Exception("Выберите функцию");
+
+                double result = funk2(x, y, functionType);
+
+                if (double.IsNaN(result) || double.IsInfinity(result))
+                    throw new Exception("Результат вычисления не определен");
 
                 Otvet.Text = result.ToString("F6");
             }
